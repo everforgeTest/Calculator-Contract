baa5c4a166355d8efbfcd8f5d00a1a05eaa91a93
everforgeTest <everforgeapiautomation@gmail.com>
2026-04-16 10:41:28 +0530

    rertrtr

--- src/startup.js
+++ src/startup.js
@@ -5,7 +5,7 @@ const bson = require('bson');
 
 const calculatorContract = async (ctx) => {
   console.log('Calculator contract is running.');
-
+//fdgdfgfh
   const isReadOnly = ctx.readonly;
 
   try {
